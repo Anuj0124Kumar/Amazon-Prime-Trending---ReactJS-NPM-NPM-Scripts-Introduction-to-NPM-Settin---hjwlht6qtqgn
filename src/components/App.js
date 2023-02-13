07189476a1792b3bd3b7ff388e90d83f2cc048fe
Anuj0124Kumar <anuj0214@gmail.com>
2023-02-13 14:59:00 +0530

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -12,25 +12,21 @@ const App = (props) => {
   }
 
   const handelPrev = () =>{
-    if(index === 0){
-      return;
-    }
+   
     setIndex(index-1);
   }
 
 const handelNext = () =>{
-   if(index === slides.length-1){
-     return;
-   }
+  
    setIndex(index+1);
 }
   return (
     <>
       <h1 data-testid="title">{props.slides[index].title}</h1>
       <p data-testid="text">{props.slides[index].text}</p>
-      <button data-testid="button-restart" onClick={handelRestart}>Restart</button>
-      <button data-testid="button-prev" onClick={handelPrev}>Prev</button>
-      <button data-testid="button-next" onClick={handelNext}>Next</button>
+      <button data-testid="button-restart" disabled={index === 0}onClick={handelRestart}>Restart</button>
+      <button data-testid="button-prev" disabled={index === 0} onClick={handelPrev}>Prev</button>
+      <button data-testid="button-next" disabled={index === slides.length-1} onClick={handelNext}>Next</button>
     </>
   )
 }
